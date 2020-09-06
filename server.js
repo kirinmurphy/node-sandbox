356e1dac09290578dcc1ea5dbb59f9cf07678cf1
Kirin <kirinmurphy@gmail.com>
2020-09-05 23:13:54 -0500

    added dotenv to local fs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+
+require('dotenv').config();
+
 const jwt = require('./app/jwt_demo');
 const posts = require('./app/resources/posts');
 const chatRooms = require('./app/resources/chatRooms');
@@ -21,3 +24,4 @@ app.use(express.static(path.join(__dirname, './app/public')));
 const PORT = process.env.PORT || 5000;
 const successMessage = `Server running on port ${PORT}`;
 server.listen(PORT, () => console.log(successMessage));
+
