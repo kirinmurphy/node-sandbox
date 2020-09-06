6f57764fd240d57fc05ee1545403bebc35b4e526
Kirin <kirinmurphy@gmail.com>
2020-09-05 22:39:32 -0500

    added mongo db url

--- app/chatbot/index.js
+++ app/chatbot/index.js
@@ -7,21 +7,18 @@ const {
   leaveRoom,
 } = require('./actions');
 
-const MONGO_DB_NAME = 'mongo_test';
-const MONGO_DB_PSWD = 'IgKLKa3iI8QJlfgk';
-const MONGO_USER = 'kmadmin';
-const MONGO_HOST = 'cluster0.cvxzn.mongodb.net';
-const connectionString = `${MONGO_USER}:${MONGO_DB_PSWD}@${MONGO_HOST}/${MONGO_DB_NAME}`;
-const uri = `mongodb+srv://${connectionString}?retryWrites=true&w=majority`;
+
+const MONGODB_URL = `${process.env.MONGODB_URL}?retryWrites=true&w=majority`;
+const MONGODB_NAME = MONGODB_URL.replace('mongodb+srv://','').split('/')[1].split('?')[0];
 
 module.exports = function (server) {
-  const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
+  const mongoClient = new MongoClient(MONGODB_URL, { useNewUrlParser: true });
   const io = socketio(server);
 
   mongoClient.connect(err => {
     if ( err ) { throw err; }
   
-    const collection = mongoClient.db(MONGO_DB_NAME).collection("chatRoom");
+    const collection = mongoClient.db(MONGODB_NAME).collection("chatRoom");
   
     io.on('connection', (socket) => {
       socket.on('joinRoom', async (user) => { 
