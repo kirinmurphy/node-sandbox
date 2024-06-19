98dfe2396d1b1e344170bbee283ba2efb8fffa0c
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-06-19 12:00:23 -0500

    style updates

--- app/public/js/chat/getMessageTemplate.js
+++ app/public/js/chat/getMessageTemplate.js
@@ -1,13 +1,20 @@
+const CHATBOT_NAME = 'CincoBot';
+
 export function getMessageTemplate({ message, currentUserName }) {
   const { username, time = '???', text } = message;
-  const isFromCurrentUser = currentUserName === username;
   const div = document.createElement('div');
   div.classList.add(`message`);
+
+  const isFromCurrentUser = currentUserName === username;
   if ( isFromCurrentUser ) div.classList.add(`me`); 
 
-  div.innerHTML = `
-    <div class="meta">${username} <span>${time}</span></div>
-    <div class="text">${text}</div>
-  `;
+  const isCincoBot = username === CHATBOT_NAME;
+  if ( isCincoBot ) div.classList.add(`bot`);
+
+  const isOtherUser = !(isFromCurrentUser || isCincoBot);
+  const displayName = isOtherUser ? username : '';
+  const displayNameHtml = isOtherUser ? `<div class="meta">${displayName}</div>` : '';
+  div.innerHTML = `<div>${displayNameHtml}</div><div class="text">${text}</div>`;
+
   return div;
 }
